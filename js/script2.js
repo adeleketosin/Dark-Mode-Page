c2b74c8a7aebddb4fa93217ba75a0819eb2ba648
Oluwatosin Rhoda Adeleke <Adeleke.O@bullseye.local>
2023-05-06 05:58:12 +0100

    Dark Mode Page

---
+++ js/script2.js
@@ -0,0 +1,15 @@
+let darkMode = false;
+
+function switchMode(){
+    if (darkMode === false){
+        darkMode = true;
+        let src = document.getElementById("icon").setAttribute("src", "/images/star_icon.png")
+        let head = document.head.innerHTML + '<link rel="stylesheet" href="/css/style2_dark.css">'
+        document.head.innerHTML = head
+    } else {
+        darkMode = false;
+        let src = document.getElementById("icon").setAttribute("src", "/images/moon-icon.png")
+        let head = document.head.innerHTML + '<link rel="stylesheet" href="/css/style2.css">'
+        document.head.innerHTML = head
+    }
+}
