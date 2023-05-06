c342223a920e7731e6a4ba6b7a5a00c178dc1bad
Oluwatosin Rhoda Adeleke <Adeleke.O@bullseye.local>
2023-05-06 19:44:04 +0100

    Dark Page

--- script2.js
+++ script2.js
@@ -3,13 +3,13 @@ let darkMode = false;
 function switchMode(){
     if(darkMode == false){
         darkMode = true;
-        let src = document.getElementById("icon").setAttribute("src", "/star_icon.png")
-        let head = document.head.innerHTML +'<link rel="stylesheet" href="/style2_dark.css">'
+        let src = document.getElementById("icon").setAttribute("src", "star_icon.png")
+        let head = document.head.innerHTML +'<link rel="stylesheet" href="style2_dark.css">'
         document.head.innerHTML = head
     } else {
         darkMode = false;
-        let src = document.getElementById("icon").setAttribute("src", "/moon-icon.png")
-        let head = document.head.innerHTML + '<link rel="stylesheet" href="/style2.css">'
+        let src = document.getElementById("icon").setAttribute("src", "moon-icon.png")
+        let head = document.head.innerHTML + '<link rel="stylesheet" href="style2.css">'
         document.head.innerHTML = head
     }
 
--- script3.js
+++ script3.js
@@ -3,13 +3,13 @@ let darkMode = false;
 function switchMode(){
     if(darkMode == false){
         darkMode = true;
-        let src = document.getElementById("icon").setAttribute("src", "/star_icon.png")
-        let head = document.head.innerHTML +'<link rel="stylesheet" href="/style3_dark.css">'
+        let src = document.getElementById("icon").setAttribute("src", "star_icon.png")
+        let head = document.head.innerHTML +'<link rel="stylesheet" href="style3_dark.css">'
         document.head.innerHTML = head
     } else {
         darkMode = false;
-        let src = document.getElementById("icon").setAttribute("src", "/moon-icon.png")
-        let head = document.head.innerHTML + '<link rel="stylesheet" href="/style3.css">'
+        let src = document.getElementById("icon").setAttribute("src", "moon-icon.png")
+        let head = document.head.innerHTML + '<link rel="stylesheet" href="style3.css">'
         document.head.innerHTML = head
     }
 
